dd5e1a58a54010e7b9a0db883e9de30c10b0ac14
christopher.barnes@ucl.ac.uk <cbarnes@physiol2.anat.ucl.ac.uk>
2017-06-27 18:15:06 +0100

    Added posterior plotting

--- analysis/do.abc.rejection.R
+++ analysis/do.abc.rejection.R
@@ -2,6 +2,23 @@ library(gtools)
 library(ggplot2)
 source("multiplot.R")
 
+#####
+## Parameters
+#####
+
+n.resim <- 100
+path <- "./simulated-data-sets/"
+path.resim <- "./resimulated-data-sets/"
+
+
+wass.dist <- function(d1,d2){
+   qpoints <- seq(0.05,0.95,0.01)
+   q1 <- quantile( d1, prob=qpoints )
+   q2 <- quantile( d2, prob=qpoints )
+   del <- (q1-q2)
+   return( sum(del^2) )
+}
+
 if(0){
    #####
    ## Access and plot the data
@@ -18,8 +35,6 @@ if(0){
    #####
    ## Access the simulations and do the ABC
    #####
-   path <- "./simulated-data-sets/"
-
    files <- list.files(path=path, pattern="results-S*")
    files <- mixedsort(sort(files))
    nfiles <- length(files)
@@ -29,7 +44,8 @@ if(0){
    for(i in 1:nfiles){
       f <- paste(path,files[i],sep="")
       sim <- read.csv(f)$RelLen
-      dists[i] <- ks.test(sim,data)$statistic
+      #dists[i] <- ks.test(sim,data)$statistic
+      dists[i] <- wass.dist(sim,data)
    }
 
    pdf("plot-fit-distances.pdf")
@@ -62,6 +78,7 @@ if(0){
    #####
    ## Extract the parameters that gave rise to these
    #####
+   top.sims <- top.sims[1:5]
 
    top.pars <- gsub("results","parameters", files[top.sims] )
    top.pars.files <-  paste(path,top.pars,sep='')
@@ -71,46 +88,74 @@ if(0){
    system(comm1)
 
    cat("running resimulation\n")
-   comm2 <- "mkdir resimulated-data-sets; cd resimulated-data-sets; ../../dynamicCIN/simulation/CINmodel Particles=100 Model=2 Resim=../posterior.dat; cd ../"
+   comm2 <- paste("mkdir resimulated-data-sets; cd resimulated-data-sets; ../../dynamicCIN/simulation/CINmodel Particles=",n.resim," Model=2 Resim=../posterior.dat; cd ../",sep="")
    system(comm2)
 }
 
-path.resim <- "./resimulated-data-sets/"
-files.resim <- list.files(path=path.resim, pattern="results-resim-S*")
 
-dens.pred.x <- c()
-dens.pred.y <- c()
+if(0){
+   #####
+   ## Resimulations: posterior predictive distribution
+   #####
+   files.resim <- list.files(path=path.resim, pattern="results-resim-S*")
 
-all.sim <- c()
+   dens.pred.x <- c()
+   dens.pred.y <- c()
 
-for(i in 1:100){
-   f <- paste(path.resim,files.resim[i],sep="")
-   sim <- read.csv(f)$RelLen
+   all.sim <- c()
 
-   # hack
-   sim <- sim[ which(sim < 5 & sim > -2) ]
-   print( summary(sim) )
+   for(i in 1:n.resim){
+      f <- paste(path.resim,files.resim[i],sep="")
+      sim <- read.csv(f)$RelLen
 
-   all.sim <- cbind(all.sim,sim)
+      # **** hack ****
+      sim <- sim[ which(sim < 5 & sim > -2) ]
+      #print( summary(sim) )
 
-   dens <- density(sim, from=-2, to=10)
-   dens.pred.x <- rbind(dens.pred.x, dens$x)
-   dens.pred.y <- rbind(dens.pred.y, dens$y)
+      all.sim <- cbind(all.sim,sim)
+
+      dens <- density(sim, from=-2, to=10)
+      dens.pred.x <- rbind(dens.pred.x, dens$x)
+      dens.pred.y <- rbind(dens.pred.y, dens$y)
+   }
+
+   dens.data <- density(data)
+   dens.pred.q <- apply(dens.pred.y,MARGIN=2,function(x){ quantile(x,p=c(0.025,0.5,0.975) ) })
+
+   xvals <- dens.pred.x[1,] 
+   dp <- data.frame( x=c(dens.data$x, xvals ), y=c(dens.data$y,dens.pred.q[2,]), z=c(rep(1,512),rep(2,512) ) )
+   #dp2 <- data.frame( x=c(xvals,xvals), y=c(dens.pred.q[1,],dens.pred.q[3,]), z=c(rep("0.025",512),rep("0.975",512))  )
+   dp3 <- data.frame( x=xvals, ymin=dens.pred.q[1,], ymax=dens.pred.q[3,] )
+
+   p <- ggplot() + geom_area(aes(x=x,y=y, fill=factor(z)), data=dp, stat="identity", alpha = 0.5, colour="black") + guides(fill=FALSE) + xlim(-2, 5) + xlab("fractional chromosomal divergence") + ylab("")
+   p <- p + geom_ribbon(aes(x=x,ymin=ymin,ymax=ymax), data=dp3, linetype="dotted", alpha=0.3)
+
+   pdf("plot-fit-post-pred-density.pdf")
+   print(p)
+   dev.off()
 }
 
-dens.data <- density(data)
-dens.pred.q <- apply(dens.pred.y,MARGIN=2,function(x){ quantile(x,p=c(0.025,0.5,0.975) ) })
+if(1){
+   
+   top.sims <- order(dists)[1:40]
 
-xvals <- dens.pred.x[1,] 
-dp <- data.frame( x=c(dens.data$x, xvals ), y=c(dens.data$y,dens.pred.q[2,]), z=c(rep(1,512),rep(2,512) ) )
-#dp2 <- data.frame( x=c(xvals,xvals), y=c(dens.pred.q[1,],dens.pred.q[3,]), z=c(rep("0.025",512),rep("0.975",512))  )
-dp3 <- data.frame( x=xvals, ymin=dens.pred.q[1,], ymax=dens.pred.q[3,] )
+   top.pars <- gsub("results","parameters", files[top.sims] )
+   top.pars.files <-  paste(path,top.pars,sep='')
 
-p <- ggplot() + geom_area(aes(x=x,y=y, fill=factor(z)), data=dp, stat="identity", alpha = 0.5, colour="black") + guides(fill=FALSE) + xlim(-2, 5) + xlab("fractional chromosomal divergence") + ylab("")
-#p <- p + geom_line(aes(x=x,y=y), linetype="dotted", data=dp2, alpha=0.2)
-p <- p + geom_ribbon(aes(x=x,ymin=ymin,ymax=ymax), data=dp3, linetype="dotted", alpha=0.1)
-#p <- p + scale_fill_manual( values = c("red","blue", "green") )
+   cat("running cat of parameter files\n")
+   comm3 <-  paste("cat", paste(top.pars.files,collapse=" "), " > posterior.dat", collapse="" )
+   system(comm3)
 
-pdf("plot-fit-post-pred-density.pdf")
-print(p)
-dev.off()
+   post <- read.table("posterior.dat",header=F)
+   names(post) <- c("Npop","ngen","g_d","mu_i","p_trans","svp2","trp2","SV_min","svgradient","svtransgrad","threshold_g","SV_max","mu_i_range","gnmdou","maxchr","minchr")
+
+   plots <- vector("list",3)
+   dp <- data.frame( p_g=post$gnmdou, p_t=post$p_trans, p_v=post$g_d )
+   plots[[1]] <- ggplot(dp, aes(x = p_v)) + geom_density(alpha = 0.5) + guides(fill=FALSE) + xlim(0, 1) + xlab("p(insertion/deletion)") + ylab("")
+   plots[[2]] <- ggplot(dp, aes(x = p_t)) + geom_density(alpha = 0.5) + guides(fill=FALSE) + xlim(0, 1) + xlab("p(translocation)") + ylab("") 
+   plots[[3]] <- ggplot(dp, aes(x = p_g)) + geom_density(alpha = 0.5) + guides(fill=FALSE) + xlim(0, 1) + xlab("p(genome doubling)") + ylab("")
+   
+   pdf("plot-posterior-1D.pdf",height=3,width=9)
+   multiplot(plotlist=plots, cols=3)
+   dev.off()
+}
